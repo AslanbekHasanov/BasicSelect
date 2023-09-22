496f4f7b4b36700e5f7333462bfb2976e2826c03
User <User@WIN-R1OMRL0S822>
2023-09-22 21:31:27 +0500

    Add project files.

---
+++ BasicSelect/Program.cs
@@ -0,0 +1,44 @@
+﻿#region string ustida amallar
+//int number;
+//string str;
+//Console.Write("number = ");
+//number = Convert.ToInt32(Console.ReadLine());
+//Console.Write("str = ");
+//str = Console.ReadLine();
+//string result = (number > str.Length) ? str.ToLower(): str.ToUpper();
+//Console.WriteLine(result);
+#endregion
+
+#region ternary dan if elsega utkazish
+//int x = 15;
+//int y = 10;
+
+////if else
+//if (x > y)
+//    Console.WriteLine("x is greater than y ");
+//else if (y > x)
+//    Console.WriteLine("x is less than y");
+//else if (x == y)
+//    Console.WriteLine("x is equal to y");
+//else
+//    Console.WriteLine("x and y are not comparable");
+#endregion
+
+#region hafta kuni nomini ingilizcha qilib chiqarish
+//string kun;
+//Console.Write("kun = ");
+//kun = Console.ReadLine();
+
+//string result = kun switch
+//{
+//    "Dushanba" => "Monday",
+//    "Seshanba" => "Tuesday",
+//    "Chorshanba" => "Wednesday",
+//    "Payshanba" => "Thursday",
+//    "Juma" => "Friday",
+//    "Shanba" => "Saturday",
+//    "Yakshanba" => "Sunday",
+//    _ =>  "qayta kiriting\n" 
+//};
+//Console.WriteLine(result);
+#endregion 
